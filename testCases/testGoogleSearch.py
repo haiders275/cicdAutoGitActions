54db220831091f90d2615e7dd6e5e54d6ce13578
haiders275 <abdullah.mkd275@gmail.com>
2024-05-10 12:50:46 +0500

    commit for Reports folder addition

--- testCases/testGoogleSearch.py
+++ testCases/testGoogleSearch.py
@@ -1,17 +1,19 @@
-import unittest
-
 import allure
-import pytest
-from selenium import webdriver
 from pageObjects.webApp_Page import GooglePage
+from utilities.customlogger import LogGen
 
 
 class TestGoogleSearchField:
+    log = LogGen.loggen()
 
     @allure.severity(allure.severity_level.NORMAL)
     def test_search_field_presence(self, setup_web):
         self.driver = setup_web
         self.driver.maximize_window()
+        self.log.info("Chrome Webdriver initialized.")
+
         self.page = GooglePage(self.driver)
         self.page.open()
+        self.log.info("Google page opened.")
+
         self.page.is_search_box_present()
--- testCases/testTitleVerification.py
+++ testCases/testTitleVerification.py
@@ -14,3 +14,30 @@ class TestGoogleSearchField:
         self.page.open()
         self.page.get_title()
         self.log.info("Title is verified.")
+
+"""
+name: Python Selenium CI Tests
+'on':
+  - push
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v2
+      - name: Set up Python
+        uses: actions/setup-python@v3
+        with:
+          python-version: 3.12
+      - name: Install dependencies
+        run: |
+          pip install -r requirements.txt
+      - name: Run tests
+        run: |
+          pytest --alluredir=./allure-results
+      - name: Upload Allure report
+        uses: actions/upload-artifact@v2
+        with:
+          name: allure-report
+          path: ./allure-results
+"""

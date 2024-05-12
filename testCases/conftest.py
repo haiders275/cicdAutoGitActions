53683ea176083df9a35161e11d2879fb775d3546
Abdullah Haider <140248857+haiders275@users.noreply.github.com>
2024-05-12 20:36:50 +0500

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -4,8 +4,22 @@ from selenium import webdriver
 
 @pytest.fixture()
 def setup_web():
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
+    # return driver
+
+    chromedriver_autoinstaller.install()
+
+    chrome_options = webdriver.ChromeOptions()    
+    options = [
+        # Define window size here
+        "--window-size=1200,1200",
+        "--ignore-certificate-errors"
+    ]
+
+    for option in options:
+    chrome_options.add_argument(option)
+
+    
+    driver = webdriver.Chrome(options = chrome_options)
     return driver
-
-
 # executable_path="../chromeDriver/chromedriver.exe"

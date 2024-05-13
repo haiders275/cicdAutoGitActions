d7f6f8f50ce7d3da418b01dacdba0eb7c4cb55bf
Abdullah Haider <140248857+haiders275@users.noreply.github.com>
2024-05-13 23:27:03 +0500

    Update testGoogleSearch.py

--- testCases/testGoogleSearch.py
+++ testCases/testGoogleSearch.py
@@ -7,7 +7,7 @@ from utilities.customlogger import LogGen
 class TestGoogleSearchField:
     log = LogGen.loggen()
     
-    @pytest.mark.sanity
+    # @pytest.mark.sanity
     @pytest.mark.regression
     @allure.severity(allure.severity_level.NORMAL)
     def test_search_field_presence(self, setup_web):
@@ -21,7 +21,7 @@ class TestGoogleSearchField:
 
         self.page.is_search_box_present()
 
-    @pytest.mark.sanity
+    # @pytest.mark.sanity
     @allure.severity(allure.severity_level.NORMAL)
     def test_search_field_presence(self, setup_web):
         self.driver = setup_web
@@ -35,7 +35,7 @@ class TestGoogleSearchField:
         self.page.is_search_box_present()
 
 
-    @pytest.mark.sanity
+    # @pytest.mark.sanity
     @allure.severity(allure.severity_level.NORMAL)
     def test_search_field_presence(self, setup_web):
         self.driver = setup_web
@@ -49,7 +49,7 @@ class TestGoogleSearchField:
         self.page.is_search_box_present()
 
 
-    @pytest.mark.regression
+    # @pytest.mark.regression
     @allure.severity(allure.severity_level.NORMAL)
     def test_search_field_presence(self, setup_web):
         self.driver = setup_web

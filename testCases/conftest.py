14ae68e0cebb8874d94080c1352edd011f5380d4
Abdullah Haider <140248857+haiders275@users.noreply.github.com>
2024-05-13 20:14:15 +0500

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-import chromedriver_autoinstaller
+# import chromedriver_autoinstaller
 
 @pytest.fixture(autouse=True)
 def setup_web(request):

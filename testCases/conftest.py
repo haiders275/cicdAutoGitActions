899e7491df3b8e8dd3c54ad6beeed37535296ec6
Abdullah Haider <140248857+haiders275@users.noreply.github.com>
2024-05-13 17:26:43 +0500

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,25 +2,34 @@ import pytest
 from selenium import webdriver
 import chromedriver_autoinstaller
 
+@pytest.fixture(autouse=True)
+def setup_web(request):
+    options=Options()
+    options.add_argument("--headless")
+    driver=webdriver.Chrome(service=Service(ChromeDriverManager().install(), options=options)
+    request.cls.driver=driver
 
-@pytest.fixture()
-def setup_web():
+    yield
+    driver.quit()
+
+# @pytest.fixture()
+# def setup_web():
     # driver = webdriver.Chrome()
     # return driver
 
-    chromedriver_autoinstaller.install()
+#     chromedriver_autoinstaller.install()
 
-    chrome_options = webdriver.ChromeOptions()    
-    options = [
-        # Define window size here
-        "--window-size=1200,1200",
-        "--ignore-certificate-errors"
-    ]
+#     chrome_options = webdriver.ChromeOptions()    
+#     options = [
+#         # Define window size here
+#         "--window-size=1200,1200",
+#         "--ignore-certificate-errors"
+#     ]
 
-    for option in options:
-        chrome_options.add_argument(option)
+#     for option in options:
+#         chrome_options.add_argument(option)
 
     
-    driver = webdriver.Chrome(options = chrome_options)
-    return driver
-# executable_path="../chromeDriver/chromedriver.exe"
+#     driver = webdriver.Chrome(options = chrome_options)
+#     return driver
+# # executable_path="../chromeDriver/chromedriver.exe"

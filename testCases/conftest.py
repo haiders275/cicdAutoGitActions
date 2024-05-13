f62ae6830d9fa949717306663e644453db33770a
Abdullah Haider <140248857+haiders275@users.noreply.github.com>
2024-05-13 17:53:37 +0500

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import chromedriver_autoinstaller
 
 @pytest.fixture(autouse=True)
 def setup_web(request):
-    # options=Options()
+    options=Options()
     options.add_argument("--headless")
     driver=webdriver.Chrome(service=Service(ChromeDriverManager().install(), options=options))
     request.cls.driver=driver

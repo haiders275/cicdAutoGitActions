101a259343e4415468f2c79ca5608fa51b489b90
Abdullah Haider <140248857+haiders275@users.noreply.github.com>
2024-05-13 23:10:16 +0500

    Update testTitleVerification.py

--- testCases/testTitleVerification.py
+++ testCases/testTitleVerification.py
@@ -5,7 +5,9 @@ from utilities.customlogger import LogGen
 
 class TestGoogleSearchField:
     log = LogGen.loggen()
-
+    
+    @pytest.mark.sanity
+    @pytest.mark.regression
     @allure.severity(allure.severity_level.NORMAL)
     def test_title_Page(self, setup_web):
         self.driver = setup_web
@@ -15,29 +17,28 @@ class TestGoogleSearchField:
         self.page.get_title()
         self.log.info("Title is verified.")
 
-"""
-name: Python Selenium CI Tests
-'on':
-  - push
-jobs:
-  test:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v2
-      - name: Set up Python
-        uses: actions/setup-python@v3
-        with:
-          python-version: 3.12
-      - name: Install dependencies
-        run: |
-          pip install -r requirements.txt
-      - name: Run tests
-        run: |
-          pytest --alluredir=./allure-results
-      - name: Upload Allure report
-        uses: actions/upload-artifact@v2
-        with:
-          name: allure-report
-          path: ./allure-results
-"""
+class TestGoogleSearchField:
+    log = LogGen.loggen()
+
+    @pytest.mark.regression
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_title_Page(self, setup_web):
+        self.driver = setup_web
+        self.driver.maximize_window()
+        self.page = GooglePage(self.driver)
+        self.page.open()
+        self.page.get_title()
+        self.log.info("Title is verified.")
+
+class TestGoogleSearchField:
+    log = LogGen.loggen()
+
+    @pytest.mark.sanity
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_title_Page(self, setup_web):
+        self.driver = setup_web
+        self.driver.maximize_window()
+        self.page = GooglePage(self.driver)
+        self.page.open()
+        self.page.get_title()
+        self.log.info("Title is verified.")
